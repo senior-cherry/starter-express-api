99e919719bc2b36851b6151b64559c4787234a8c
Олександр Пархоменко <vt211_poye@student.ztu.edu.ua>
2023-04-26 21:46:21 +0300

    Change 11

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
+const {expressjwt: jwt} = require("express-jwt");
 
 router.get(`/`, async (req, res) => {
     const userList = await User.find().select('-passwordHash');
@@ -90,5 +91,26 @@ router.post('/login', async (req, res) => {
 
     res.status(200).send(user);
 });
+
+router.post('/register', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+        return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
 
 module.exports = router;

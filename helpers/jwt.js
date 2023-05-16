139cb487993256a50281650e09c76238f5bdf917
Олександр Пархоменко <vt211_poye@student.ztu.edu.ua>
2023-05-16 15:24:57 +0300

    commented string with products

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -48,7 +48,7 @@ const authJWT = () => {
     }).unless({
         path: [
             {url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS']},
-            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            //{url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST']},
             '/api/v1/users/login',
